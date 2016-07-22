f1f32da89852b31863c1ed2a56b379dcc948aa0a
Matthias Erll <matthias@erll.de>
2016-07-22 08:33:57 +0200

    Added readme to setup.

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
+import os
+
+from distutils.spawn import find_executable
 from setuptools import setup, find_packages
 
 from docker_registry_util import __version__
+
+
+def include_readme():
+    try:
+        import pandoc
+    except ImportError:
+        return ''
+    pandoc.core.PANDOC_PATH = find_executable('pandoc')
+    readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+    doc = pandoc.Document()
+    with open(readme_file, 'r') as rf:
+        doc.markdown = rf.read().encode()
+        return doc.rst
 
 
 setup(
@@ -8,11 +24,12 @@ setup(
     version=__version__,
     packages=find_packages(),
     install_requires=['setuptools', 'requests'],
-    url='',
+    url='https://github.com/merll/docker-registry-util',
     license='MIT',
     author='Matthias Erll',
     author_email='matthias@erll.de',
     description='Search and cleanup on Docker Registry v2.',
+    long_description=include_readme(),
     platforms=['OS Independent'],
     keywords=['docker', 'registry', 'query', 'cleanup'],
     classifiers=[
